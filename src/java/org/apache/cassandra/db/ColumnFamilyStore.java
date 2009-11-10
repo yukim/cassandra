e76499f8e5de56b7e9cfb90b5933d2becca46969
Chris Goffinet <goffinet@apache.org>
2009-11-10 05:13:56 +0000

    forceFlushBinary() was calling memtable not binaryMemtable_ patch by goffinet; reviewed by jbellis for CASSANDRA-532
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cassandra/branches/cassandra-0.4@834349 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -423,7 +423,7 @@ public final class ColumnFamilyStore implements ColumnFamilyStoreMBean
 
     public void forceFlushBinary()
     {
-        if (memtable_.isClean())
+        if (binaryMemtable_.get().isClean())
             return;
 
         submitFlush(binaryMemtable_.get());
